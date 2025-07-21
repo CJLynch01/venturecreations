cea633fa04e2221d0829d84d58461b16fa269d29
Christopher Lynch <cjlynch@byui.edu>
2025-07-21 17:58:20 -0600

    Save

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -15,7 +15,10 @@ passport.use(
   new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/auth/google/callback"
+    callbackURL: process.env.NODE_ENV === "production"
+      ? "https://venturecreations.net/auth/google/callback"
+      : "http://localhost:3000/auth/google/callback"
+
   }, async (accessToken, refreshToken, profile, done) => {
     try {
       let user = await User.findOne({ googleId: profile.id });
